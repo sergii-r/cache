4a35f45c3b6f10968db9bd85be0428f2fa79ad44
Sergii Rodymenko <sergii.rodymenko@gmail.com>
2022-12-27 17:49:20 +0200

    add Get

--- cache.go
+++ cache.go
@@ -10,6 +10,10 @@ func (c Cache) Set(key string, value interface{}) {
 	c[key] = value
 }
 
+func (c Cache) Get(key string) interface{} {
+	return c[key]
+}
+
 func (c Cache) Delete(key string) {
 	delete(c, key)
 }
